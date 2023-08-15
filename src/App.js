423e7903da4b23f549fc533fb1068bcc2b97f343
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-08-16 02:04:59 +0500

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
 import "./App.css";
 
 import {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
