f1a68c2c4827f98815c14d5973c1f1bb4dc03760
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-08-15 11:31:11 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: ["#39AD96"],
+      },
+    },
+    fontFamily: {
+      signature: ["Sacramento"],
+    },
+  },
+  plugins: [],
+};
