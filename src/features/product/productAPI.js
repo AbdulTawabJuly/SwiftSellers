334bdfbdeeba32d7fc9bfa90526ae381c632d816
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-08-16 21:58:02 +0500

    coomit 5

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,6 +1,19 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080");
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter) {
+  let query = "";
+  for (let key in filter) {
+    query += `${key}=${filter[key]}&`;
+  }
+  //& is liye use kiya hai lyu take aik se zyada objects ko bhi handle kar sake
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products?" + query);
     const data = await response.json();
     resolve({ data });
   });
