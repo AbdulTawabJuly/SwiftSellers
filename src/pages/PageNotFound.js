0f7298d1b84b18b8bf8d99f5f49f47a2d9fff308
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-08-22 23:09:33 +0500

    Commit 19

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound() {
     <main className="grid min-h-screen  place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h1 className="mt-4 text-3xl font-bold font-signature tracking-tight text-gray-900 sm:text-5xl">
           Page Not Found
           <img
             src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
