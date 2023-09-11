b4bab30caff3a4e2cc676e9ead54aef15ec3e70c
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-09-12 00:51:54 +0500

    Commit 33

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -36,6 +36,8 @@ function AdminOrders() {
         return "bg-yellow-200 text-yellow-600";
       case "delivered":
         return "bg-green-200 text-green-600";
+      case "received":
+        return "bg-green-200 text-green-600";
       case "cancelled":
         return "bg-red-200 text-red-600";
       default:
@@ -44,8 +46,14 @@ function AdminOrders() {
   };
 
   const handleShow = () => {};
-  const handleUpdate = (e, order) => {
+  const handleOrderStatus = (e, order) => {
     const updatedOrder = { ...order, status: e.target.value };
+    dispatch(updateOrderAsync(updatedOrder));
+    setEditableOrderId(-1);
+  };
+
+  const handleOrderPaymentStatus = (e, order) => {
+    const updatedOrder = { ...order, paymentStatus: e.target.value };
     dispatch(updateOrderAsync(updatedOrder));
     setEditableOrderId(-1);
   };
@@ -68,10 +76,10 @@ function AdminOrders() {
     <>
       {/* component */}
       <div className="overflow-x-auto">
-        <div className=" bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+        <div className=" bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
           <div className="w-full">
             <div className="bg-white shadow-md rounded my-6">
-              <table className="min-w-max w-full table-auto">
+              <table className="w-full table-auto">
                 <thead>
                   <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
                     <th
@@ -110,7 +118,9 @@ function AdminOrders() {
                         ))}
                     </th>
                     <th className="py-3 px-6 text-center">Shipping Address</th>
-                    <th className="py-3 px-6 text-center">Status</th>
+                    <th className="py-3 px-6 text-center">Order Status</th>
+                    <th className="py-3 px-6 text-center">Payment Method</th>
+                    <th className="py-3 px-6 text-center">Payment Status</th>
                     <th className="py-3 px-6 text-center">Actions</th>
                   </tr>
                 </thead>
@@ -180,7 +190,7 @@ function AdminOrders() {
 
                       <td className="py-3 px-6 text-center">
                         {order.id === editableOrderId ? (
-                          <select onChange={(e) => handleUpdate(e, order)}>
+                          <select onChange={(e) => handleOrderStatus(e, order)}>
                             <option value="pending">Pending</option>
                             <option value="dispatched">Dispatched</option>
                             <option value="delivered">Delivered</option>
@@ -196,6 +206,32 @@ function AdminOrders() {
                           </span>
                         )}
                       </td>
+
+                      <td className="py-3 px-6 text-center">
+                        <div className="flex items-center justify-center">
+                          {order.paymentMethod}
+                        </div>
+                      </td>
+
+                      <td className="py-3 px-6 text-center">
+                        {order.id === editableOrderId ? (
+                          <select
+                            onChange={(e) => handleOrderPaymentStatus(e, order)}
+                          >
+                            <option value="pending">Pending</option>
+                            <option value="received">Received</option>
+                          </select>
+                        ) : (
+                          <span
+                            className={`${chooseColor(
+                              order.paymentStatus
+                            )} py-1 px-3 rounded-full text-xs`}
+                          >
+                            {order.paymentStatus}
+                          </span>
+                        )}
+                      </td>
+
                       <td className="py-3 px-6 text-center">
                         <div className="flex item-center justify-center">
                           <div className="w-6 mr-4 transform hover:text-purple-500 hover:scale-120">
